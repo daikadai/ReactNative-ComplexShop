0c59d7af74acbf8d79ce964f6aa38a269e66a587
Pham Cao Dai <daikadai1026@gmail.com>
2020-10-08 16:50:30 +0700

    12. SideDrawer && The Orders Screen

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,22 +1,70 @@
+import React from 'react';
 import { Platform } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
-import { createAppContainer } from 'react-navigation';
+import { createAppContainer } from "react-navigation";
+import { createDrawerNavigator } from "react-navigation-drawer";
 import Colors from "../constants/Colors";
 import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
 import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
 import CartScreen from "../screens/shop/CartScreen";
+import OrderScreen from "../screens/shop/OrderScreen";
+import { Ionicons } from "@expo/vector-icons";
 
-const ProductNavigator = createStackNavigator({
-  ProductsOverview: ProductsOverviewScreen,
-  ProductDetail: ProductDetailScreen,
-  Cart: CartScreen
-}, {
-  defaultNavigationOptions: {
-    headerStyle: {
-      backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+const defaultNavOptions = {
+  headerStyle: {
+    backgroundColor: Platform.OS === "android" ? Colors.primary : "",
+  },
+  headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
+};
+
+const ProductNavigator = createStackNavigator(
+  {
+    ProductsOverview: ProductsOverviewScreen,
+    ProductDetail: ProductDetailScreen,
+    Cart: CartScreen,
+  },
+  {
+    navigationOptions: {
+      drawerIcon: (drawerConfig) => (
+        <Ionicons
+          name={Platform.OS === "android" ? "md-cart" : "ios-cart"}
+          size={23}
+          color={drawerConfig.tintColor}
+        />
+      ),
     },
-    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+    defaultNavigationOptions: defaultNavOptions,
   }
-})
+);
 
-export default createAppContainer(ProductNavigator)
+const OrdersNavigator = createStackNavigator(
+  {
+    Orders: OrderScreen,
+  },
+  {
+    navigationOptions: {
+      drawerIcon: (drawerConfig) => (
+        <Ionicons
+          name={Platform.OS === "android" ? "md-list" : "ios-list"}
+          size={23}
+          color={drawerConfig.tintColor}
+        />
+      ),
+    },
+    defaultNavigationOptions: defaultNavOptions,
+  }
+);
+
+const ShopNavigator = createDrawerNavigator(
+  {
+    Products: ProductNavigator,
+    Orders: OrdersNavigator,
+  },
+  {
+    contentOptions: {
+      activeTintColor: Colors.primary,
+    },
+  }
+);
+
+export default createAppContainer(ShopNavigator);
--- screens/shop/OrderScreen.js
+++ screens/shop/OrderScreen.js
@@ -1,12 +1,35 @@
-import React from 'react'
-import { FlatList } from 'react-native'
+import React from "react";
+import { FlatList, Platform, Text } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../../components/UI/HeaderButton";
+import { useSelector } from "react-redux";
 
 const OrderScreen = () => {
+  const orders = useSelector((state) => state.orders.orders);
   return (
-    <div>
-      <FlatList />
-    </div>
-  )
+    <FlatList
+      data={orders}
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => <Text>{itemData.item.totalAmount}</Text>}
+    />
+  );
+};
+
+OrderScreen.navigationOptions = navData => {
+  return {
+    headerTitle: 'Your Orders',
+    headerLeft: ()=> (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName={Platform.OS === "android" ? "md-menu" : "ios-menu"}
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    )
+  }
 }
 
-export default OrderScreen
+export default OrderScreen;
