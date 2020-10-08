fe6b12a50ffbe065cf1f0f3e8c35ea40393d9d05
Pham Cao Dai <daikadai1026@gmail.com>
2020-10-08 17:33:55 +0700

    14. Styling Order Items

--- screens/shop/OrderScreen.js
+++ screens/shop/OrderScreen.js
@@ -3,6 +3,7 @@ import { FlatList, Platform, Text } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../../components/UI/HeaderButton";
 import { useSelector } from "react-redux";
+import OrderItem from "../../components/shop/OrderItem";
 
 const OrderScreen = () => {
   const orders = useSelector((state) => state.orders.orders);
@@ -10,15 +11,20 @@ const OrderScreen = () => {
     <FlatList
       data={orders}
       keyExtractor={(item) => item.id}
-      renderItem={(itemData) => <Text>{itemData.item.totalAmount}</Text>}
+      renderItem={(itemData) => (
+        <OrderItem
+          amount={itemData.item.totalAmount}
+          date={itemData.item.readableDate}
+        />
+      )}
     />
   );
 };
 
-OrderScreen.navigationOptions = navData => {
+OrderScreen.navigationOptions = (navData) => {
   return {
-    headerTitle: 'Your Orders',
-    headerLeft: ()=> (
+    headerTitle: "Your Orders",
+    headerLeft: () => (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
         <Item
           title="Menu"
@@ -28,8 +34,8 @@ OrderScreen.navigationOptions = navData => {
           }}
         />
       </HeaderButtons>
-    )
-  }
-}
+    ),
+  };
+};
 
 export default OrderScreen;
