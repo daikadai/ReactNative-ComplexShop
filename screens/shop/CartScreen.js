8d56a7891b1b302cc3dff948fb3acdd3434e44ff
Pham Cao Dai <daikadai1026@gmail.com>
2020-10-08 12:15:41 +0700

    9. Outputting Cart Items

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,34 +1,49 @@
 import React from "react";
 import { Button, FlatList, StyleSheet, Text, View } from "react-native";
 import { useSelector } from "react-redux";
+import CartItem from "../../components/shop/CartItem";
 import Colors from "../../constants/Colors";
 
 const CartScreen = () => {
   const cartTotalAmount = useSelector((state) => state.cart.totalAmount);
-  const cartItems = useSelector(state => {
+  const cartItems = useSelector((state) => {
     const transformedCartItems = [];
-    for( const key in state.cart.items) {
+    for (const key in state.cart.items) {
       transformedCartItems.push({
         productId: key,
         productTitle: state.cart.items[key].productTitle,
         productPrice: state.cart.items[key].productPrice,
         quantity: state.cart.items[key].quantity,
-        sum: state.cart.items[key].sum
-      })
+        sum: state.cart.items[key].sum,
+      });
     }
     return transformedCartItems;
-  })
+  });
   return (
     <View style={styles.screen}>
       <View style={styles.summary}>
         <Text style={styles.summaryText}>
-          Total: <Text style={styles.amount}>${cartTotalAmount.toFixed(2)}</Text>
+          Total:{" "}
+          <Text style={styles.amount}>${cartTotalAmount.toFixed(2)}</Text>
         </Text>
-        <Button color={Colors.accent} title="Order Now" disabled={cartItems.length === 0}/>
+        <Button
+          color={Colors.accent}
+          title="Order Now"
+          disabled={cartItems.length === 0}
+        />
       </View>
-      <View>
-        <Text>CART ITEMS</Text>
-      </View>
+      <FlatList
+        data={cartItems}
+        keyExtractor={(item) => item.productId}
+        renderItem={(itemData) => (
+          <CartItem
+            quantity={itemData.item.quantity}
+            title={itemData.item.productTitle}
+            amount={itemData.item.sum}
+            onRemove={() => {}}
+          />
+        )}
+      />
     </View>
   );
 };
@@ -37,10 +52,10 @@ const styles = StyleSheet.create({
   screen: {
     margin: 20,
   },
-  summary: {  
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+  summary: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
     marginBottom: 20,
     padding: 10,
     shadowColor: "black",
@@ -52,12 +67,12 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
   },
   summaryText: {
-    fontFamily: 'open-sans-bold',
-    fontSize: 18
+    fontFamily: "open-sans-bold",
+    fontSize: 18,
   },
   amount: {
-    color: Colors.primary
-  }
+    color: Colors.primary,
+  },
 });
 
 export default CartScreen;
